e74f1df848425e186eb0c432e4e31c99686bc991
analogsoul13 <analogsoul13@gmail.com>
2022-12-17 23:29:41 +0530

    Depname field now holds FullName of Department

--- bdfweb/department/models.py
+++ bdfweb/department/models.py
@@ -6,13 +6,13 @@ from django.contrib.auth.models import User
 
 class Department (models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    depname = models.CharField(max_length=100)
+    depname = models.CharField(max_length=100, default="")
     contact = models.CharField(max_length=15, null=True)
     address = models.CharField(max_length=300)
     deppic = models.FileField(null=True)
     deppin = models.CharField(max_length=6, null=True)
     depidpic = models.FileField(null=True)
-    aboutdep = models.CharField(max_length=300)
+    aboutdep = models.CharField(max_length=600)
     status = models.CharField(max_length=20)
     regdate = models.DateTimeField(auto_now_add=True)
     adminremark = models.CharField(max_length=500)
